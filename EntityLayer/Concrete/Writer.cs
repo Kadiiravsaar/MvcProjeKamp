4d190d1d0ef9f423d5029853706beefcfbe3a7f5
kadiiravsaar <kadiiravsar@gmail.com>
2022-09-29 16:44:49 +0300

    Dal Abstract file added. Concrete file added

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,26 @@ namespace EntityLayer.Concrete
 {
     public class Writer
     {
+        [Key] // bunlara attribute denir
         public int WriterID { get; set; }
+
+        [StringLength(50)] // bunlara attribute denir
         public string WriterName { get; set; }
+
+        [StringLength(50)] // bunlara attribute denir
         public string WriterSurName { get; set; }
+
+        [StringLength(1000)]
         public string WriterImage { get; set; }
+
+        [StringLength(50)]
         public string WriterMail { get; set; }
+
+        [StringLength(20)]
         public string WriterPassword { get; set; }
-        
+
+        public ICollection<Heading> Heading { get; set; }
+        public ICollection<Content> Content { get; set; }
+
     }
 }
